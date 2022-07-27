2e63702b345d86804b32ad811901da029723f6ab
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-07-27 15:58:08 -0500

    Binary tree project

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -5,8 +5,17 @@
  * nodes with at least 1 child in a binary tree
  * @tree: is a pointer to the root node of
  * the tree to count the number of nodes
- * 
+ *
  * Return: If tree is NULL return 0
  */
 
-size_t binary_tree_nodes(const binary_tree_t *tree);
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if ((tree->left == NULL && tree->right == NULL))
+		return (0);
+
+	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
+}
