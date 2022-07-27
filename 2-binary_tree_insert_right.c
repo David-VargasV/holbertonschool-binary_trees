5e1e56148bd2263167b51b6182311a4ec129c682
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-07-27 13:45:01 -0500

    Check task 2 whitout NULL

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,7 +11,7 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newNode = NULL;
+	binary_tree_t *newNode;
 
 	newNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 	if (parent == NULL)
